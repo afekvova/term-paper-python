f72769a2a97e615b14e9cb0ffe37b334e7e0759b
Kefa Vova <kefavova24@gmail.com>
2022-12-07 20:44:15 +0200

    Спроба використати PyQt6

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QPushButton, QWidget
+from tkinter import *
+from tkinter import messagebox
 
-app = QApplication([])
-window = QWidget()
-window.setFixedWidth(400)
-window.setFixedHeight(600)
-window.setWindowTitle("Система управління бібліотекою")
-layout = QVBoxLayout()
+from pymongo import MongoClient
 
-art_alb = QLabel()
-art_alb.setText("Перевірка")
-layout.addWidget(art_alb, Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
-window.setLayout(layout)
-
-window.show()
-app.exec()
+# Спроба використати PyQt6
+# from PyQt6.QtCore import Qt
+# from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QPushButton, QWidget
+#
+# app = QApplication([])
+# window = QWidget()
+# window.setFixedWidth(400)
+# window.setFixedHeight(600)
+# window.setWindowTitle("Система управління бібліотекою")
+# layout = QVBoxLayout()
+#
+# art_alb = QLabel()
+# art_alb.setText("michael buble - christmas")
+# layout.addWidget(art_alb, Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+#
+# layout.addWidget(QPushButton('Top'))
+# layout.addWidget(QPushButton('Bottom'))
+# window.setLayout(layout)
+#
+# window.show()
+# app.exec()
